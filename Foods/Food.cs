c82f2b6fc1dc1f76cb05fe9884631874132499a4
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2022-11-30 19:50:56 +0100

    Moved MenuOfTheDay method to new class that will be used to generate all menus

--- Foods/Food.cs
+++ Foods/Food.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleItalianRestaurant.Foods.FoodsFactory;
 using ConsoleItalianRestaurant.Foods.IFoods;
 
 namespace ConsoleItalianRestaurant.Foods
@@ -12,16 +11,5 @@ namespace ConsoleItalianRestaurant.Foods
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
-
-        private static List<Food> menu = FoodFactroy.MenuOfTheDay();
-
-        public static void FoodsMenu()
-        {
-            Console.WriteLine($"Todays menu ({DateTime.Now:d}):");
-            foreach (Food item in menu)
-            {
-                Console.WriteLine(item.ToString());
-            }
-        }
     }
 }
---
+++ Restaurant/Menus/Menu.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleItalianRestaurant.Foods.FoodsFactory;
+using ConsoleItalianRestaurant.Foods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleItalianRestaurant.Restaurant.Menus
+{
+    internal sealed class Menu
+    {
+        private static Menu instance = null;
+        public static Menu Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Menu();
+                }
+                return instance;
+            }
+        }
+
+        private List<Food> menu;
+
+        public Menu()
+        {
+            menu = FoodFactroy.MenuOfTheDay();
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine($"Todays menu ({DateTime.Now:d}):");
+            foreach (Food item in menu)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
